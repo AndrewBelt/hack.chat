25ed09de898d8a58a9092a13d56b39272b31b241
Benjamin Armstrong <benjamin.armstrong92@gmail.com>
2015-07-12 19:34:58 -0500

    Fixed question mark in latex issue. closes #39

--- client/client.js
+++ client/client.js
@@ -151,7 +151,7 @@ function pushMessage(nick, text, time, cls) {
 	textEl.classList.add('text')
 
 	textEl.textContent = text || ''
-	textEl.innerHTML = textEl.innerHTML.replace(/(\?|https?:\/\/)\S+?(?=[,.!?:)]?\s|$)/g, parseLinks)
+	textEl.innerHTML = textEl.innerHTML.replace(/( \?|^\?|https?:\/\/)\S+?(?=[,.!?:)]?\s|$)/g, parseLinks)
 	try {
 		renderMathInElement(textEl, {delimiters: [
 		  {left: "$$", right: "$$", display: true},
@@ -186,16 +186,17 @@ function send(data) {
 }
 
 
-function parseLinks(g0) {
+function parseLinks(g0, match) {
 	var a = document.createElement('a')
-	a.innerHTML = g0
+	a.innerHTML = g0.trim()
 	var url = a.textContent
 	if (url[0] == '?') {
 		url = "/" + url
 	}
 	a.href = url
 	a.target = '_blank'
-	return a.outerHTML
+	if(match == ' ?') return ' ' + a.outerHTML
+	else return a.outerHTML
 }
 
 
